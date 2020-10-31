8b10e74f7af7a57f65170c59dcc19853514f90de
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-10-31 08:31:53 -0400

    finished feature and list issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,25 @@
 var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
+var queryString = document.location.search;
+var repoNameEl = document.querySelector("#repo-name");
+
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
     fetch(apiUrl).then(function(response) {
@@ -16,12 +36,13 @@ var getRepoIssues = function(repo) {
           });
         }
         else {
-          alert("There was a problem with your request!");
+          //if not successful, redirect to homepage
+          document.location.replace("./index.html");
         }
       });
     }
 
-getRepoIssues("facebook/react");
+getRepoIssues();
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -68,3 +89,5 @@ var displayIssues = function(issues) {
             //add text to warning container
 
         };
+
+        getRepoName();
